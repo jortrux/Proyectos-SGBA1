d7cc3bc430315c3ac60f4fe6aa355e3d1a9c493c
David <david.ca.2993@gmail.com>
2025-03-22 12:27:13 +0100

    Enhance climate data processing: rename columns, handle NaN values, and merge with price data

--- data/data_cleaning/datos_clima/juntar_clima_precio.py
+++ data/data_cleaning/datos_clima/juntar_clima_precio.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import StandardScaler
 
 def rename_columns(df, idema):
     for col in df.columns:
-        if col != 'fecha':
+        if col != 'fecha' and col not in ['anio', 'mes', 'dia', 'dia_semana', 'estacion']:
             # Renombramos las columnas para que no haya colisiones
             df.rename(columns={col: f"{col}_{idema}"}, inplace=True)
     return df
@@ -52,8 +52,8 @@ df_clima.sort_values('fecha', inplace=True)
 df_clima.reset_index(drop=True, inplace=True)
 
 print(f"Guardando el dataframe con el clima de todas las estaciones")
-df_clima.to_parquet('data/processed/datos_clima/clima_numerizado_all.parquet')
-df_clima.to_csv('data/processed/datos_clima/clima_numerizado_all.csv', index=False)
+df_clima.to_parquet('data/processed/datos_clima/clima_numerizado_all_NaN.parquet')
+df_clima.to_csv('data/processed/datos_clima/clima_numerizado_all_NaN.csv', index=False)
 
 # Procesar valores NaN
 lista_estaciones = ['3196', '9434', '9170', '2539', '6155A', '5514', '1505', '3469A', '8019', '0200E', '4121', '8175']
@@ -67,8 +67,25 @@ for estacion in lista_estaciones:
     # Contar NaN en las columnas de esta estación antes del procesamiento
     num_nan_before = df_clima[cols_estacion].isna().sum().sum()
     print(f"Estación {estacion}: {num_nan_before} valores NaN antes del procesamiento KNN")
-        
+    # Ver cantidad de NaN por columna para esta estación
+    nan_counts = df_clima[cols_estacion].isna().sum()
+    print(f"Desglose de NaN por columna para estación {estacion}:")
+    for col, count in nan_counts.items():
+        print(f"  {col}: {count} NaN")
+
     if cols_estacion:
+
+        horaHrMax_col = f"horaHrMax_{estacion}"
+        horaHrMin_col = f"horaHrMin_{estacion}"
+        
+        # Impute with the mode
+        if horaHrMax_col in df_clima.columns:
+            mode_value = df_clima[horaHrMax_col].mode()[0]
+            df_clima[horaHrMax_col] = df_clima[horaHrMax_col].fillna(mode_value)
+        if horaHrMin_col in df_clima.columns:
+            mode_value = df_clima[horaHrMin_col].mode()[0]
+            df_clima[horaHrMin_col] = df_clima[horaHrMin_col].fillna(mode_value)
+
         # Escalar
         scaler_estacion = StandardScaler()
         datos_estacion = df_clima[cols_estacion].fillna(df_clima[cols_estacion].mean())
@@ -89,5 +106,36 @@ for estacion in lista_estaciones:
         print(f"Estación {estacion}: {num_nan_after} valores NaN después del procesamiento KNN")
 
 
+print("NaN en df de clima despues de procesamiento KNN")
+print(df_clima.isna().sum().sum())
+# # Mostrar columnas con NaN y su cantidad
+# nan_cols = df_clima.columns[df_clima.isna().any()].tolist()
+# print("\nColumnas con NaN:")
+# for col in nan_cols:
+#     nan_count = df_clima[col].isna().sum()
+#     print(f"{col}: {nan_count} NaN")
 
+# Merge con el clima
+df_precio = pd.read_csv('data/processed/datos_precio/precio_consumo_electrico_timestamp_media.csv')
+df_precio['timestamp'] = pd.to_datetime(df_precio['timestamp'])
+df_precio['fecha'] = df_precio['timestamp'].dt.date
+df_precio.drop(['timestamp', 'consumo_kwh', 'coste_euros'], axis=1, inplace=True)
+
+df_precio['fecha'] = pd.to_datetime(df_precio['fecha'])
+df_clima['fecha'] = pd.to_datetime(df_clima['fecha'])
+
+df_merged = pd.merge(df_precio, df_clima, on='fecha', how='left')
+df_merged.dropna()
+df_merged.sort_values('fecha', inplace=True)
+df_merged.dropna(inplace=True)
+df_merged.reset_index(drop=True, inplace=True)
+
+
+print(f"Guardando el dataframe con el clima de todas las estaciones")
+df_clima.to_parquet('data/processed/datos_clima/clima_numerizado_all.parquet')
+df_clima.to_csv('data/processed/datos_clima/clima_numerizado_all.csv', index=False)
+
+print(f"Guardando el dataframe con el clima y precio de todas las estaciones")
+df_merged.to_parquet('data/processed/datos_precio/clima_precio_merged.parquet')
+df_merged.to_csv('data/processed/datos_precio/clima_precio_merged.csv', index=False)
 
