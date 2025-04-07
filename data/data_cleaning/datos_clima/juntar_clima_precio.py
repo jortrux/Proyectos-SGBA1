e5e80d6751e4d9c3953c9cc3ddd7b876765dd620
David <david.ca.2993@gmail.com>
2025-04-07 18:38:32 +0200

    Refactor data merging script: update price data source and column names
    - Changed the source CSV file for price data from 'precio_consumo_electrico_timestamp_media.csv' to 'pvpc.csv'.
    - Renamed columns for clarity: 'Timestamp' to 'timestamp' and 'PVPC' to '€/kwh'.
    - Commented out the drop operation for 'consumo_kwh' and 'coste_euros' columns.
    - Added a print statement to display merged DataFrame columns.

--- data/data_cleaning/datos_clima/juntar_clima_precio.py
+++ data/data_cleaning/datos_clima/juntar_clima_precio.py
@@ -116,10 +116,14 @@ print(df_clima.isna().sum().sum())
 #     print(f"{col}: {nan_count} NaN")
 
 # Merge con el clima
-df_precio = pd.read_csv('data/processed/datos_precio/precio_consumo_electrico_timestamp_media.csv')
+df_precio = pd.read_csv('data/processed/datos_precio/pvpc.csv')
+
+df_precio.rename(columns={'Timestamp': 'timestamp'}, inplace=True)
+df_precio.rename(columns={'PVPC': '€/kwh'}, inplace=True)
+
 df_precio['timestamp'] = pd.to_datetime(df_precio['timestamp'])
 df_precio['fecha'] = df_precio['timestamp'].dt.date
-df_precio.drop(['consumo_kwh', 'coste_euros'], axis=1, inplace=True)
+# df_precio.drop(['consumo_kwh', 'coste_euros'], axis=1, inplace=True)
 
 df_precio['fecha'] = pd.to_datetime(df_precio['fecha'])
 df_clima['fecha'] = pd.to_datetime(df_clima['fecha'])
@@ -151,3 +155,5 @@ for particula in lista_particulas_eliminar:
 
 df_merged.to_parquet('data/processed/datos_precio/clima_precio_merged_recortado.parquet')
 df_merged.to_csv('data/processed/datos_precio/clima_precio_merged_recortado.csv', index=False)
+
+print(df_merged.columns)
